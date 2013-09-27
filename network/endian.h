cd8589330673e614ea259a9d8d318b83ce2285a4
Mark Fassler <mfassler@users.noreply.github.com>
2013-09-28 00:33:30 +0900

    more parentheses to avoid compiler warnings

--- network/endian.h
+++ network/endian.h
@@ -5,10 +5,10 @@
 
 // Network is big-endian, but we are little-endian
 
-#define ntohs(x) (( (x & 0xff00) >> 8 ) | ( (x & 0x00ff) << 8))
-#define htons(x) (( (x & 0xff00) >> 8 ) | ( (x & 0x00ff) << 8))
+#define ntohs(x) ( (((x) & 0xff00) >> 8 ) | (((x) & 0x00ff) << 8) )
+#define htons(x) ( (((x) & 0xff00) >> 8 ) | (((x) & 0x00ff) << 8) )
 
-#define ntohl(x) (  ((x & 0xff000000) >> 24) | ((x & 0x00ff0000) >> 8) | ((x & 0x0000ff00) << 8) | ((x & 0x000000ff) << 24) )
-#define htonl(x) (  ((x & 0xff000000) >> 24) | ((x & 0x00ff0000) >> 8) | ((x & 0x0000ff00) << 8) | ((x & 0x000000ff) << 24) )
+#define ntohl(x) ( (((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >> 8) | (((x) & 0x0000ff00) << 8) | (((x) & 0x000000ff) << 24) )
+#define htonl(x) ( (((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >> 8) | (((x) & 0x0000ff00) << 8) | (((x) & 0x000000ff) << 24) )
 
 
